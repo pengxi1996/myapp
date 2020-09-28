b30ac8c55b203b6e6471193b22bc632bbde418e3
pengx <pengxi199607@163.com>
2020-09-28 11:26:46 +0800

    完成到第五步添加路由

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
-
+import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,15 +9,22 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Hero组件选择了id为${hero.id}的人员`)
 
   }
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.check();
+  }
+  check(): void {
+    this.heroService.getHeroes().subscribe(data => {
+      this.heroes = data;
+    })
 
   }
 }
